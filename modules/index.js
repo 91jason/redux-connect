5cf4042c405d093fe51da2add59b8b4405a26a7d
91jasoncao <91jasoncao@gmail.com>
2017-02-16 16:50:53 +0800

    redux-connect-ie8 1.0.0

--- modules/index.js
+++ modules/index.js
@@ -3,4 +3,6 @@ import { asyncConnect } from './containers/decorator';
 import { loadOnServer } from './helpers/utils';
 import { reducer, immutableReducer } from './store';
 import { setToImmutableStateFunc, setToMutableStateFunc } from './helpers/state';
-export { ReduxAsyncConnect,asyncConnect,loadOnServer,reducer, immutableReducer,setToImmutableStateFunc, setToMutableStateFunc};
+
+export { ReduxAsyncConnect, asyncConnect, loadOnServer };
+export { reducer, immutableReducer, setToImmutableStateFunc, setToMutableStateFunc };
